5699e28e7c7b8373e6dc09fb3a52ee409d986452
bhmurphy <bhm5zr@virginia.edu>
2018-02-17 15:41:07 -0500

    fix urls.py

--- app/textbookexchange/myapp/urls.py
+++ app/textbookexchange/myapp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from myapp import views
 
 urlpatterns = [
-    url(r'^v1/professors/create/$', views.create_professor, name='create-new-professor-api')
+    url(r'^v1/professors/(?P<pk>[-\w]+)/$', views.details_professor, name='get-professor-api'),
+    url(r'^v1/professors/create/$', views.create_professor, name='create-new-professor-api'),
+    url(r'^v1/professors/(?P<pk>[-\w]+)/delete/$', views.delete_professor, name='delete-professor-api')
 ]
