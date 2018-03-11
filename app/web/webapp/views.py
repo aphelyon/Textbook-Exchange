c49b7c0572694a00f02e9d6cee8a2081b71b948e
bhmurphy <bhm5zr@virginia.edu>
2018-03-11 17:15:29 -0400

    fix web to accomodate for api changes

--- app/web/webapp/views.py
+++ app/web/webapp/views.py
@@ -15,10 +15,6 @@ def listing_view(request, pk):
     experience_request = urllib.request.Request(request_url)
     # This is from the code in the write-up
     listing_details = json.loads(urllib.request.urlopen(experience_request).read().decode('utf-8'))
-    if 'error' in listing_details:
-        listing_details['does_not_exist'] = True
-        return render(request, 'listing_detail.html', listing_details)
-    else:
-        listing_details['listing']['item']['pub_date'] = datetime.strptime(listing_details['listing']['item']['pub_date'][:-6], "%Y-%m-%d %H:%M:%S")
-
-        return render(request, 'listing_detail.html', listing_details['listing'])
+    if 'results' in listing_details:
+        listing_details['results']['item']['pub_date'] = datetime.strptime(listing_details['results']['item']['pub_date'][:-6], "%Y-%m-%d %H:%M:%S")
+    return render(request, 'listing_detail.html', listing_details)
