4dcdb1437fe38471f8b17bcd123333b9b673abb1
bhmurphy <bhm5zr@virginia.edu>
2018-03-12 19:16:23 -0400

    change index url

--- app/web/webapp/urls.py
+++ app/web/webapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from webapp import views
 
 
-urlpatterns = [url(r'^home$', views.index, name='index'),
+urlpatterns = [url(r'^$', views.index, name='index'),
                url(r'^listing/(?P<pk>\d+)$', views.listing_view, name='listing'),
                url(r'^user/(?P<pk>\d+)$', views.user_profile_view, name='user'),
                url(r'^course/(?P<pk>\d+)$', views.course_view, name='course'),
