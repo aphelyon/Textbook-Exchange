7363d1817c7f57362242ac571eba70fb0c98a2fd
bhmurphy <bhm5zr@virginia.edu>
2018-03-19 15:06:15 -0400

    login now adds a cookie when successful

--- app/web/webapp/views.py
+++ app/web/webapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 import urllib.request
+import urllib.parse
 import json
 from datetime import datetime
+import webapp.forms
 
 
 # Create your views here.
@@ -49,3 +51,28 @@ def textbook_view(request, pk):
     if 'textbook' in textbook_details:
         textbook_details['textbook']['results']['pub_date'] = datetime.strptime(textbook_details['textbook']['results']['pub_date'][:-6], "%Y-%m-%d %H:%M:%S")
     return render(request, 'textbook_detail.html', textbook_details)
+
+
+def login(request):
+    form = webapp.forms.LoginForm()
+    if request.method == "GET":
+        return render(request, 'login.html', {'form': form})
+
+    ret_form = webapp.forms.LoginForm(request.POST)
+
+    if not ret_form.is_valid():
+        return render(request, 'login.html', {'form': form})
+
+    username = ret_form.cleaned_data['username']
+    password = ret_form.cleaned_data['password']
+    experience_url = 'http://exp-api:8000/experience/login'
+    data = urllib.parse.urlencode({'username': username, 'password': password}).encode('utf-8')
+    experience_request = urllib.request.Request(experience_url, data)
+    experience_response = json.loads(urllib.request.urlopen(experience_request).read().decode('utf-8'))
+    if not experience_response or not experience_response['ok']:
+        return render(request, 'login.html', {'form': form})
+    authenticator = experience_response['results']['authenticator']
+    response = HttpResponseRedirect(reverse('webapp:index'))
+    response.set_cookie("auth", authenticator)
+
+    return response
