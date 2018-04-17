99f6ff3328eb86cd2ba64cc52a5535896e6503e4
Siteng <sz4dd@virginia.edu>
2018-04-16 22:26:58 -0400

    modified the selenium script to start testing
    basis is there, requires lot more testing though

--- app/selenium/Selenium.py
+++ app/selenium/Selenium.py
@@ -1,15 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from django.test import TestCase, Client
+import unittest
+
+class SeleniumTest(unittest.TestCase):
+    fixtures = ['selenium/db.json', ]
+
+    def setUp(self):
+        self.driver = webdriver.Remote(
+        command_executor='http://192.168.99.100:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME)
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://165.227.202.249:8000")
+        self.assertIn("Textbook Exchange", driver.title)
 
 
-driver = webdriver.Remote(
-   command_executor='http://192.168.99.100:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.CHROME)
-#driver = webdriver.Chrome()
-driver.get("http://165.227.202.249:8000")
-if not "Textbook Exchange" in driver.title:
-    raise Exception("Unable to load page!")
-if "Textbook Exchange" in driver.title:
-    print("yeahyeah")
+    def tearDown(self):
+        self.driver.close()
 
+if __name__ == "__main__":
+    unittest.main()
